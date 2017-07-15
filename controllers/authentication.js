b36b9b203d566e8861731c6bd27a384eb6781051
Manny Blum <manny.blum@gmail.com>
2017-07-15 00:17:55 -0400

    added method to add user to mongo if new, send 422 error if already exists

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -6,12 +6,24 @@ exports.signup = function(req, res, next) {
 
   // See if user with the given email exists
   User.findOne({ email: email }, function(err, existingUser) {
+    if (err) { return next(err); }
 
+    // If a user with email does exist, return an error
+    if (existingUser) {
+      return res.status(422).send({ error: 'Email is in use' });
+    }
+
+    // If a user with email does NOT exist, create and save user record
+    const user = new User({
+      email: email,
+      password: password
+    });
+
+    user.save(function(err) {
+      if (err) { return next(err); }
+
+      // Respond to request indicating the user was created
+      res.json(user);
+    });
   });
-
-  // If a user with email does exist, return an error
-
-  // If a user with email does NOT exist, create and save user record
-
-  // Respond to request indicating the user was created
 };
